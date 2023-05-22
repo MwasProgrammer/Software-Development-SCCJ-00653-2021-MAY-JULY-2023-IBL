4479466416542f7df2275fb62b8512903df2c3e1
MwasProgrammer <gichimupeter07@gmail.com>
2023-05-22 21:06:11 +0300

    AquaGroove frontend

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>index</title>
+</head>
+<body>
+    <div id="box">
+        <header class="main-header">
+            <h1>AquaGroove<span><img src="img/drop.jpg" height="50" width="50"></h1>
+        </header>
+        <nav id = "navbar">
+            <div class="container">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Records</a></li>
+                    <li><a href="#">Achievements</a></li>
+                    <li><a href="#">Settings</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <section id="main-section">
+            <h2>Welcome Peter,</h2>
+            <p>Below is the summary of your water intake.</p>
+            <div class="container">
+                <div id= "box1">
+                    <p>Number of cups drunk today</p>
+                    <h3>24</h3>
+                </div>
+                <div id="box2">
+                    <p>Total cups</p>
+                    <h3>3200</h3>
+                </div>
+            
+            </div>
+            <br>
+            <div id= "cup">
+                <h4>Water intake</h4>
+                <div id="box3">
+                    <div class="water-cup">
+                        <img src="img/cup.jpg" height="250" width="250">
+                        <p>My intake</p>
+                        <h3>24</h3>
+                    </div>
+                </div>
+            </div>
+            <div></div>
+        </section>
+    </div>
+
+    
+    
+</body>
+</html>
